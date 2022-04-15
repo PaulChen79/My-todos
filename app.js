adbc1dc4b6927b06f01373c7e5fa6d6086bbaa3c
Pin Hsu Chen <paul791111@gmail.com>
2022-04-15 13:51:49 +0800

    bug waiting for fixing

--- app.js
+++ app.js
@@ -2,8 +2,7 @@ const express = require("express")
 const exphbs = require("express-handlebars")
 const methodOverride = require("method-override")
 const mongoose = require("mongoose")
-const Todo = require("./models/todo")
-const methodOverride = require("method-override")
+const routes = require("./routes")
 const app = express()
 require("dotenv").config()
 
@@ -22,67 +21,7 @@ app.set("view engine", "hbs")
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride, "_method")
 
-app.get("/", (req, res) => {
-    Todo.find()
-        .lean()
-        .sort({ _id: "asc" })
-        .then(todos => res.render("index", { todos }))
-        .catch(error => console.error(error))
-
-})
-
-// Home page
-app.get("/todos/new", (req, res) => {
-    return res.render("new")
-})
-
-// create todo method
-app.post("/todos", (req, res) => {
-    const name = req.body.name
-    return Todo.create({ name })
-        .then(() => res.redirect("/"))
-        .catch(error => console.error(error))
-})
-
-// read detail method
-app.get("/todos/:id", (req, res) => {
-    const id = req.params.id
-    return Todo.findById(id)
-        .lean()
-        .then((todo) => res.render("detail", { todo }))
-        .catch(error => console.error(error))
-})
-
-// update todo method
-app.get("/todos/:id/edit", (req, res) => {
-    const id = req.params.id
-    return Todo.findById(id)
-        .lean()
-        .then((todo) => res.render("edit", { todo }))
-        .catch(error => console.error(error))
-})
-
-app.put("/todos/:id", (req, res) => {
-    const id = req.params.id
-    const { name, isDone } = req.body
-    return Todo.findById(id)
-        .then((todo) => {
-            todo.name = name
-            todo.isDone = isDone === "on"
-            return todo.save()
-        })
-        .then(() => res.redirect(`/todos/${id}`))
-        .catch(error => console.error(error))
-})
-
-// delete todo method
-app.delete("/todos/:id", (req, res) => {
-    const id = req.params.id
-    return Todo.findById(id)
-        .then((todo) => todo.remove())
-        .then(() => res.redirect("/"))
-        .catch(error => console.error(error))
-})
+app.use(routes)
 
 app.listen(3000, () => {
     console.log("Server is listening on port 3000");
