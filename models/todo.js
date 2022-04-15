73ce740205483512f4833897c3054b038ddc6d2a
Pin Hsu Chen <paul791111@gmail.com>
2022-04-15 11:16:17 +0800

    Model todo add isDone params

--- models/todo.js
+++ models/todo.js
@@ -3,6 +3,10 @@ const todoSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true
+    },
+    isDone: {
+        type: Boolean,
+        default: false
     }
 })
 
