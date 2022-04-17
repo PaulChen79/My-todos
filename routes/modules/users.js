2cc80654fb474ba1219eb520d2c87e9a14c77463
Pin Hsu Chen <paul791111@gmail.com>
2022-04-17 10:51:04 +0800

    Add user model

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -5,4 +5,16 @@ router.get('/login', (req, res) => {
     res.render("login")
 })
 
+router.post('/login', (req, res) => {
+
+})
+
+router.get('/register', (req, res) => {
+    res.render("register")
+})
+
+router.post('/register', (req, res) => {
+    res.render("register")
+})
+
 module.exports = router
